3b82330c4355411eac8edab78805c51ce0071ddf
NikitaSaitov <126459734+NikitaSaitov@users.noreply.github.com>
2023-04-10 14:17:21 +0100

    all const should be present now

--- const.h
+++ const.h
@@ -63,7 +63,7 @@ const int NO_SIDE_TO_MOVE = -1;
 const std::string PIECE_INDEX_TO_ASCII = "PNBRQKpnbrqk";
 
 const std::string START_POSITION_FEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
-const std::string TEST_POSITIONS_FEN[3] = {
+const std::string TEST_POSITIONS_FEN[3]{
     "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1",
     "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - 0 1",
     "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1"
