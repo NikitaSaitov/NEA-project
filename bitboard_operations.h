775b7d7708131161492952a86818fd52d7b9dcca
NikitaSaitov <126459734+NikitaSaitov@users.noreply.github.com>
2023-05-04 09:29:26 +0100

    Add comments, ensure WASM compatibility with C linkage

--- bitboard_operations.h
+++ bitboard_operations.h
@@ -2,70 +2,90 @@
 #define BITBOARD_OPERATIONS_H
 
 #include <iostream>
-#include "engine_exceptions.h"
+#include <exception>
 
-using U64 = unsigned long long;
+extern "C" {
 
-inline void setBit(U64& bitboard, int squareIndex)
-{
-    bitboard |= (1ULL << squareIndex);
-}
+    using U64 = unsigned long long;
 
-inline void popBit(U64& bitboard, int squareIndex)
-{
-    bitboard & (1ULL << squareIndex) ? bitboard ^= (1ULL << squareIndex) : 0;
-}
-
-inline int getBit(U64 bitboard, int squareIndex)
-{
-    return (bitboard & (1ULL << squareIndex))? 1 : 0;
-}
-
-inline int getPopulationCount(U64 bitboard)
-{
-    int populationCount = 0;
-    
-    while(bitboard){
-        populationCount++;
-        bitboard &= bitboard - 1;
+    //Set a bit at the given square index on the given bitboard
+    inline void setBit(U64& bitboard, int squareIndex){
+        bitboard |= (1ULL << squareIndex);
     }
-    return populationCount;
-}
 
-inline int getLS1BIndex(U64 bitboard)
-{
-    if(bitboard){
-        //Two's complement trick to isolate the LS1B, -1 to get the index 
-        return getPopulationCount((bitboard & -bitboard) - 1);
-    //If an empty bitboard is passed
-    }else{
-        throw LSBOfEmptyBitboardException();
+    //Pop the bit at the given square index on the given bitboard
+    inline void popBit(U64& bitboard, int squareIndex){
+        bitboard & (1ULL << squareIndex) ? bitboard ^= (1ULL << squareIndex) : 0;
     }
-}
 
-inline void printBitboard(const U64& bitboard){
+    //Get a bit at the given square index on the given bitboard
+    inline int getBit(U64 bitboard, int squareIndex){
+        return (bitboard & (1ULL << squareIndex))? 1 : 0;   
+    }
 
-    std::cout << '\n' << "Visual representation: " << "\n\n";
-    
-    for(int rank = 0; rank < 8; rank++){
+    //Get the cardinality of the given bitboard
+    inline int getPopulationCount(U64 bitboard){
 
-        for(int file = 0; file < 8; file++){
+        int populationCount = 0;
+        
+        //While there are bits on the bitboard
+        while(bitboard){
 
-            //Least significant file (LSF) mapping
-            int squareIndex = rank * 8 + file;
+            //Increment the populationCount variable
+            populationCount++;
+            //Remove the LS1B of the bitboard
+            bitboard &= bitboard - 1;
 
-            //Print the ranks
-            if(!file){
-                std::cout << 8 - rank << "  ";
-            }
-            std::cout << (getBit(bitboard, squareIndex)? 1 : 0) << ' ';
         }
-        std::cout << '\n';
+
+        return populationCount;
+
     }
-    //Print the files
-    std::cout << '\n' << "   a b c d e f g h " << '\n';
-    //Display the decimal equivalent of a bitboard
-    std::cout << '\n' << "Decimal representation: " << bitboard << '\n';
+
+    //Get the LS1B if of the given bitboard
+    inline int getLS1BIndex(U64 bitboard){
+        
+        //If a non-empty bitboard is passed
+        if(bitboard){
+            //Bit manipulation to get the population count the trailing bits
+            return getPopulationCount((bitboard & -bitboard) - 1);
+        //If an empty bitboard is passed
+        }else{
+            throw std::invalid_argument("Invalid bitboard: empty bitboard");
+        }
+
+    }
+
+    //Print the given bitboard 
+    inline void printBitboard(const U64& bitboard){
+
+        std::cout << '\n' << "Visual representation: " << "\n\n";
+        
+        for(int rank = 0; rank < 8; rank++){
+
+            for(int file = 0; file < 8; file++){
+
+                //Least significant file (LSF) mapping
+                int squareIndex = rank * 8 + file;
+
+                //Print the ranks
+                if(!file){
+                    std::cout << 8 - rank << "  ";
+                }
+
+                std::cout << (getBit(bitboard, squareIndex)? 1 : 0) << ' ';
+
+            }
+            std::cout << '\n';
+        }
+
+        //Print the files
+        std::cout << '\n' << "   a b c d e f g h " << '\n';
+        //Display the decimal equivalent of a bitboard
+        std::cout << '\n' << "Decimal representation: " << bitboard << '\n';
+
+    }
+
 }
 
 #endif
