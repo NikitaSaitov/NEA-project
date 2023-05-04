4b68974f22267d602b7e2c8ea21acd21c9d13f0d
NikitaSaitov <126459734+NikitaSaitov@users.noreply.github.com>
2023-05-04 09:25:51 +0100

    Add comments, ensure WASM compatibility with C linkage

--- AttackTable.h
+++ AttackTable.h
@@ -1,40 +1,64 @@
-using U64 = unsigned long long;
+extern "C" {
 
-class AttackTable
-{
-    private:
-    
-        U64 pawnAttacks[2][64];
-        U64 knightAttacks[64];
-        U64 kingAttacks[64];
-        U64 bishopAttacks[64][512];
-        U64 rookAttacks[64][4096];
+    using U64 = unsigned long long;
 
-        U64 bishopMasks[64];
-        U64 rookMasks[64];
+    class AttackTable{
+        
+        private:
+        
+            U64 pawnAttacks[2][64];
+            U64 knightAttacks[64];
+            U64 kingAttacks[64];
 
-        U64 bishopMagics[64];
-        U64 rookMagics[64];
+            //The maximum relevant bits for a bishop is 9, so the number of possible occupancy combinatons is 2^9 = 512
+            U64 bishopAttacks[64][512];
 
-        void initializeMagicNumbers();
-        void initializeLeapingPieceTables();
-        void initializeSlidingPieceTables(bool fBishop);
+            //The maximum relevant bits for a bishop is 11, so the number of possible occupancy combinations is 2^11 = 4096
+            U64 rookAttacks[64][4096];
 
-    public:
+            U64 bishopMasks[64];
+            U64 rookMasks[64];
 
-        AttackTable(){
-            
-            initializeMagicNumbers();
-            initializeLeapingPieceTables();
-            initializeSlidingPieceTables(true);
-            initializeSlidingPieceTables(false);
-        }
+            U64 bishopMagics[64] = {0};
+            U64 rookMagics[64] = {0};
 
-        const U64 getPawnAttacks(int sideToMove, int squareIndex);
-        const U64 getKnightAttacks(int squareIndex);
-        const U64 getKingAttacks(int squareIndex);
-        const U64 getBishopAttacks(int squareIndex, U64 occupancy);
-        const U64 getRookAttacks(int squareIndex, U64 occupancy);
-        const U64 getQueenAttacks(int squareIndex, U64 occupancy);      
-};
+            //Initialise magic numbers
+            void initialiseMagicNumbers();
 
+            //Initialise leaping piece attack tables
+            void initialiseLeapingPieceTables();
+
+            //Initialise sliding piece attack tables
+            void initialiseSlidingPieceTables(bool fBishop);
+
+        public:
+
+            //Class constructor to initialise magic numbers and piece attacks
+            AttackTable(){
+                
+                initialiseMagicNumbers();
+                initialiseLeapingPieceTables();
+                initialiseSlidingPieceTables(true);
+                initialiseSlidingPieceTables(false);
+
+            }
+
+            //Get pawn attacks
+            const U64 getPawnAttacks(int sideToMove, int squareIndex);
+
+            //Get knight attacks
+            const U64 getKnightAttacks(int squareIndex);
+
+            //Get king attacks
+            const U64 getKingAttacks(int squareIndex);
+
+            //Get bishop attacks
+            const U64 getBishopAttacks(int squareIndex, U64 occupancy);
+
+            //Get rook attacks
+            const U64 getRookAttacks(int squareIndex, U64 occupancy);
+
+            //Get queen attacks
+            const U64 getQueenAttacks(int squareIndex, U64 occupancy);      
+    };
+}
