73c29e2a1db27629a0b1421b18d775028fcef2e8
NikitaSaitov <126459734+NikitaSaitov@users.noreply.github.com>
2023-05-04 10:14:41 +0100

    Few minor fixes, add comments, ensure WASM compatibility with C linkage

--- magics.cpp
+++ magics.cpp
@@ -1,285 +1,272 @@
-#include <cstring>
-#include "magics.h"
+#include "masks.h"
 #include "bitboard_operations.h"
-#include "engine_exceptions.h"
-#include "random.h"
 #include "const.h"
 
-enum {white, black, both};
+extern "C" {
 
-U64 maskPawnAttacks(int squareIndex, int sideToMove)
-{
-    U64 pawnsBitboard = 0ULL, attacksBitboard = 0ULL;
-    setBit(pawnsBitboard, squareIndex);
+    enum {white, black, both};
 
-    switch (sideToMove)
-    {
-    case white:
+    //Mask the pawn attacks for a given square and color
+    U64 maskPawnAttacks(int squareIndex, int colour){
 
-        if(pawnsBitboard & NOT_H_FILE){
-            attacksBitboard |= (pawnsBitboard >> 7);
-        }
-        if(pawnsBitboard & NOT_A_FILE){
-            attacksBitboard |= (pawnsBitboard >> 9);
-        }
-        break;
+        U64 pawnsBitboard = 0ULL, attacksBitboard = 0ULL;
+        setBit(pawnsBitboard, squareIndex);
 
-    case black:
+        switch (colour)
+        {
 
-        if(pawnsBitboard & NOT_H_FILE){
-            attacksBitboard |= (pawnsBitboard << 9);
-        }
-        if(pawnsBitboard & NOT_A_FILE){
-            attacksBitboard |= (pawnsBitboard << 7);
-        }
-        break;
-    }
-    return attacksBitboard;
-}
+        //If the pawn is white
+        case white:
 
-U64 maskKnightAttacks(int squareIndex)
-{
-    U64 knightsBitboard = 0ULL, attacksBitboard = 0ULL;
-    setBit(knightsBitboard, squareIndex);
-
-    if(knightsBitboard & NOT_H_FILE){
-            attacksBitboard |= (knightsBitboard >> 15);
-    }
-    if(knightsBitboard & NOT_A_FILE){
-            attacksBitboard |= (knightsBitboard >> 17);
-    }
-    if(knightsBitboard & NOT_HG_FILE){
-            attacksBitboard |= (knightsBitboard >> 6);
-    }
-    if(knightsBitboard & NOT_AB_FILE){
-            attacksBitboard |= (knightsBitboard >> 10);
-    }
-    if(knightsBitboard & NOT_A_FILE){
-            attacksBitboard |= (knightsBitboard << 15);
-    }
-    if(knightsBitboard & NOT_H_FILE){
-            attacksBitboard |= (knightsBitboard << 17);
-    }
-    if(knightsBitboard & NOT_AB_FILE){
-            attacksBitboard |= (knightsBitboard << 6);
-    }
-    if(knightsBitboard & NOT_HG_FILE){
-            attacksBitboard |= (knightsBitboard << 10);
-    }
-    return attacksBitboard;
-}
-
-U64 maskBishopAttacks(int squareIndex)
-{
-    U64 attacksBitboard = 0ULL;
-
-    int rank, file;
-
-    int targetRank = squareIndex / 8;
-    int targetFile = squareIndex % 8;
-
-    for(rank = targetRank + 1, file  = targetFile + 1; rank < 7 && file < 7; rank++, file++){
-        setBit(attacksBitboard, rank * 8 + file);
-    }
-    for(rank = targetRank - 1, file = targetFile + 1; rank > 0 && file < 7; rank--, file++){
-        setBit(attacksBitboard, rank * 8 + file);
-    }
-    for(rank = targetRank + 1, file = targetFile - 1; rank < 7 && file > 0; rank++, file--){
-        setBit(attacksBitboard, rank * 8 + file);
-    }
-    for(rank = targetRank - 1, file = targetFile - 1; rank > 0 && file > 0; rank--, file--){
-        setBit(attacksBitboard, rank * 8 + file);
-    }
-    return attacksBitboard;
-}
-
-U64 maskRookAttacks(int squareIndex)
-{
-    U64 attacksBitboard = 0ULL;
-
-    int rank, file;
-
-    int targetRank = squareIndex / 8;
-    int targetFile = squareIndex % 8;
-
-    for(rank = targetRank + 1; rank < 7; rank++){
-        setBit(attacksBitboard, rank * 8 + targetFile);
-    }
-    for(rank = targetRank - 1; rank > 0; rank--){
-        setBit(attacksBitboard, rank * 8 + targetFile);
-    }
-    for(file = targetFile + 1; file < 7; file++){
-        setBit(attacksBitboard, targetRank * 8 + file);
-    }
-    for(file = targetFile - 1; file > 0; file--){
-        setBit(attacksBitboard, targetRank * 8 + file);
-    }
-    return attacksBitboard;
-}
-
-U64 maskKingAttacks(int squareIndex)
-{
-    U64 kingBitboard = 0ULL, attacksBitboard = 0ULL;
-    setBit(kingBitboard, squareIndex);
-
-    if(kingBitboard >> 8){
-        attacksBitboard |= kingBitboard >> 8;
-    }
-    if(kingBitboard & NOT_H_FILE){
-        attacksBitboard |= kingBitboard >> 7;
-    }
-    if(kingBitboard & NOT_A_FILE){
-        attacksBitboard |= kingBitboard >> 9;
-        attacksBitboard |= kingBitboard >> 1;
-    }
-    if(kingBitboard << 8){
-        attacksBitboard |= kingBitboard << 8;
-    }
-    if(kingBitboard & NOT_A_FILE){
-        attacksBitboard |= kingBitboard << 7;
-    }
-    if(kingBitboard & NOT_H_FILE){
-        attacksBitboard |= kingBitboard << 9;
-        attacksBitboard |= kingBitboard << 1;
-    }
-    return attacksBitboard;
-}
-
-U64 generateBishopAttacks(int squareIndex, const U64& occupancy)
-{
-    U64 attacksBitboard = 0ULL;
-    
-    int rank, file;
-
-    int targetRank = squareIndex / 8;
-    int targetFile = squareIndex % 8;
-
-    for(rank = targetRank + 1, file  = targetFile + 1; rank < 8 && file < 8; rank++, file++){
-        setBit(attacksBitboard, rank * 8 + file);
-        if(getBit(occupancy, rank * 8 + file)){
-            break;
-        }
-    }
-    for(rank = targetRank - 1, file = targetFile + 1; rank >= 0 && file < 8; rank--, file++){
-        setBit(attacksBitboard, rank * 8 + file);
-        if(getBit(occupancy, rank * 8 + file)){
-            break;
-        }
-    }
-    for(rank = targetRank + 1, file = targetFile - 1; rank < 8 && file >= 0; rank++, file--){
-        setBit(attacksBitboard, rank * 8 + file);
-        if(getBit(occupancy, rank * 8 + file)){
-            break;
-        }
-    }
-    for(rank = targetRank - 1, file = targetFile - 1; rank >= 0 && file >= 0; rank--, file--){
-        setBit(attacksBitboard, rank * 8 + file);
-        if(getBit(occupancy, rank * 8 + file)){
-            break;
-        }
-    }
-    return attacksBitboard;
-}
-
-U64 generateRookAttacks(int squareIndex, const U64& occupancy)
-{
-    U64 attacksBitboard = 0ULL;
-
-    int rank, file;
-
-    int targetRank = squareIndex / 8;
-    int targetFile = squareIndex % 8;
-
-    for(rank = targetRank + 1; rank < 8; rank++){
-        setBit(attacksBitboard, rank * 8 + targetFile);
-        if(getBit(occupancy, rank * 8 + targetFile)){
-            break;
-        }
-    }
-    for(rank = targetRank - 1; rank >= 0; rank--){
-        setBit(attacksBitboard, rank * 8 + targetFile);
-        if(getBit(occupancy, rank * 8 + targetFile)){
-            break;
-        }
-    }
-    for(file = targetFile + 1; file < 8; file++){
-        setBit(attacksBitboard, targetRank * 8 + file);
-        if(getBit(occupancy, targetRank * 8 + file)){
-            break;
-        }
-    }
-    for(file = targetFile - 1; file >= 0; file--){
-        setBit(attacksBitboard, targetRank * 8 + file);
-        if(getBit(occupancy, targetRank * 8 + file)){
-            break;
-        }
-    }
-    return attacksBitboard;
-}
-
-U64 generateOccupancy(int occupancyIndex, int relevantBits, U64 attackMask)
-{
-    U64 bitboard = 0ULL;
-
-    for (int currentBit = 0; currentBit < relevantBits; currentBit++){
-        
-        int squareIndex = getLS1BIndex(attackMask);
-        popBit(attackMask, squareIndex);
-
-        if (occupancyIndex & (1 << currentBit)){
-            setBit(bitboard, squareIndex);
-        }
-    }
-    return bitboard;
-}
-
-U64 findMagicNumber(int squareIndex, int relevantBits, bool fBishop)
-{
-    U64 occupancies[4096], attacks[4096], usedAttacks[4096];
-    //Assign the attack mask
-    U64 attackMask = fBishop ? maskBishopAttacks(squareIndex) : maskRookAttacks(squareIndex);
-    //The position of the piece impacts the amount of squares it controls, which influences the number of possible occupancies
-    int maxOccupancyIndex = 1 << relevantBits;
-
-    for(int index = 0; index < maxOccupancyIndex; index++){
-        occupancies[index] = generateOccupancy(index, relevantBits, attackMask);
-        attacks[index] = fBishop ? generateBishopAttacks(squareIndex, occupancies[index]) : generateRookAttacks(squareIndex, occupancies[index]);
-    }
-
-    seedRandom();
-
-    //Trial and error method needs a lot of repetitions, values from 10^7 to 10^9 should be used
-    for(int i = 0; i < 100000000; i++){
-
-        U64 magicNumber  = getRandomFewBits();
-        //Skip the magic if the magic index is too large
-        if(getPopulationCount((attackMask * magicNumber) & 0xFF00000000000000) < 6){
-            continue;
-        }
-
-        memset(usedAttacks, 0, sizeof(usedAttacks));
-
-        int index;
-        bool fFail;
-
-        for(index = 0, fFail = false; !fFail && index < maxOccupancyIndex; index++){
-
-            //Generate the magic number and destroy the garbage bits
-            int magicIndex = (int)((occupancies[index] * magicNumber) >> (64 - relevantBits));
-
-            //Test the magic number
-            if(usedAttacks[magicIndex] == 0ULL){
-                usedAttacks[magicIndex] = attacks[index];
-            }else if(usedAttacks[magicIndex] != attacks[index]){
-                fFail = true;
+            //If the pawn is not located on the h (rightmost) file 
+            if(pawnsBitboard & NOT_H_FILE){
+                //Shift the bit on the pawn bitboard up one row and one square to the right, then add that bit to the attacks bitboard
+                attacksBitboard |= (pawnsBitboard >> 7);
             }
+
+            //If the pawn is not located on the a (leftmost) file
+            if(pawnsBitboard & NOT_A_FILE){
+                //Shift the bit on the pawn bitboard up one row and to one square the left, then add that bit to the attacks bitboard
+                attacksBitboard |= (pawnsBitboard >> 9);
+            }
+
+            break;
+
+        //If the pawn is black
+        case black:
+
+            //If the pawn is not located on the h (rightmost) file 
+            if(pawnsBitboard & NOT_H_FILE){
+                //Shift the bit on the pawn bitboard down one row and one square to the left, then add that bit to the attacks bitboard
+                attacksBitboard |= (pawnsBitboard << 9);
+            }
+
+            //If the pawn is not located on the a (leftmost) file
+            if(pawnsBitboard & NOT_A_FILE){
+                //Shift the bit on the pawn bitboard down one row and one square to the right, then add that bit to the attacks bitboard
+                attacksBitboard |= (pawnsBitboard << 7);
+            }
+
+            break;
+
         }
 
-        if(!fFail){
-            return magicNumber;
-        }
+        return attacksBitboard;
+
     }
 
-    throw CannotFindMagicNumberException();
-    return 0ULL;
+    //Mask the knight attacks for a given square
+    U64 maskKnightAttacks(int squareIndex){
+
+        U64 knightsBitboard = 0ULL, attacksBitboard = 0ULL;
+        setBit(knightsBitboard, squareIndex);
+
+        //If the knight is not located on the h (rightmost) file 
+        if(knightsBitboard & NOT_H_FILE){
+
+            //Shift the bit on the knight bitboard up two rows and one square to the right, then add that bit to the attacks bitboard
+            attacksBitboard |= (knightsBitboard >> 15);
+
+            //Shift the bit on the knight bitboard down two rows and one square to the right, then add that bit to the attacks bitboard
+            attacksBitboard |= (knightsBitboard << 17);
+
+        }
+
+        //If the knight is not located on the a (leftmost) file
+        if(knightsBitboard & NOT_A_FILE){
+
+            //Shift the bit on the knight bitboard up two rows and one square to the left, then add that bit to the attacks bitboard
+            attacksBitboard |= (knightsBitboard >> 17);
+            
+            //Shift the bit on the knight bitboard down two rows and one square to the left, then add that bit to the attacks bitboard
+            attacksBitboard |= (knightsBitboard << 15);
+
+        }
+
+        //If the knight is not located on the h or g (two rightmost) files
+        if(knightsBitboard & NOT_HG_FILE){
+
+            //Shift the bit on the knight bitboard up one row and two squares to the right, then add that bit to the attacks bitboard
+            attacksBitboard |= (knightsBitboard >> 6);
+
+            //Shift the bit on the knight bitboard down one row and two squares to the right, then add that bit to the attacks bitboard
+            attacksBitboard |= (knightsBitboard << 10);
+
+        }
+
+        //If the knight is not located on the a or b (two leftmost) files
+        if(knightsBitboard & NOT_AB_FILE){
+
+            //Shift the bit on the knight bitboard up one row and two squares to the left, then add that bit to the attacks bitboard
+            attacksBitboard |= (knightsBitboard >> 10);
+
+            //Shift the bit on the knight bitboard down one row and two squares to the left, then add that bit to the attacks bitboard
+            attacksBitboard |= (knightsBitboard << 6);
+        }
+
+        return attacksBitboard;
+
+    }
+
+    //Mask the bishop attacks for a given square
+    U64 maskBishopAttacks(int squareIndex){
+
+        U64 attacksBitboard = 0ULL;
+
+        int rank, file;
+
+        //Get the file and the rank from the square index
+        int targetRank = squareIndex / 8;
+        int targetFile = squareIndex % 8;
+
+        //Cast an attack ray in the positive file and positive rank direction
+        for(rank = targetRank + 1, file  = targetFile + 1; rank < 7 && file < 7; rank++, file++){
+            //Set the bit on the attack bitboard
+            setBit(attacksBitboard, rank * 8 + file);
+        }
+
+        //Cast an attack ray in the positive file and negative rank direction
+        for(rank = targetRank - 1, file = targetFile + 1; rank > 0 && file < 7; rank--, file++){
+            //Set the bit on the attack bitboard
+            setBit(attacksBitboard, rank * 8 + file);
+        }
+
+        //Cast an attack ray in the negative file and positive rank direction
+        for(rank = targetRank + 1, file = targetFile - 1; rank < 7 && file > 0; rank++, file--){
+            //Set the bit on the attack bitboard
+            setBit(attacksBitboard, rank * 8 + file);
+        }
+
+        //Cast an attack ray in the negative file and negative rank direction
+        for(rank = targetRank - 1, file = targetFile - 1; rank > 0 && file > 0; rank--, file--){
+            //Set the bit on the attack bitboard
+            setBit(attacksBitboard, rank * 8 + file);
+        }
+
+        return attacksBitboard;
+
+    }
+
+    //Mask the rook attacks for a given square
+    U64 maskRookAttacks(int squareIndex){
+
+        U64 attacksBitboard = 0ULL;
+
+        int rank, file;
+
+        //Get the file and the rank from the square index
+        int targetRank = squareIndex / 8;
+        int targetFile = squareIndex % 8;
+
+        //Cast an attack ray in the positive rank direction
+        for(rank = targetRank + 1; rank < 7; rank++){
+            //Set the bit on the attack bitboard
+            setBit(attacksBitboard, rank * 8 + targetFile);
+        }
+
+        //Cast an attack ray in the negative rank direction
+        for(rank = targetRank - 1; rank > 0; rank--){
+            //Set the bit on the attack bitboard
+            setBit(attacksBitboard, rank * 8 + targetFile);
+        }
+
+        //Cast an attack ray in the positive file direction
+        for(file = targetFile + 1; file < 7; file++){
+            //Set the bit on the attack bitboard
+            setBit(attacksBitboard, targetRank * 8 + file);
+        }
+
+        //Cast an attack ray in the negative file direction
+        for(file = targetFile - 1; file > 0; file--){
+            //Set the bit on the attack bitboard
+            setBit(attacksBitboard, targetRank * 8 + file);
+        }
+
+        return attacksBitboard;
+
+    }
+
+    //Mask the king attacks for a given square
+    U64 maskKingAttacks(int squareIndex){
+
+        U64 kingBitboard = 0ULL, attacksBitboard = 0ULL;
+        setBit(kingBitboard, squareIndex);
+
+        //If moving the king up one row keeps it on the board 
+        if(kingBitboard >> 8){
+            //Shift the bit on the king bitboard up one row, then add that bit to the attacks bitboard
+            attacksBitboard |= kingBitboard >> 8;
+        }
+
+        //If moving the king down one row keeps it on the board 
+        if(kingBitboard << 8){
+            //Shift the bit on the king bitboard up one row, then add that that bit to the attacks bitboard
+            attacksBitboard |= kingBitboard << 8;
+        }
+        
+        //If the knight is not located on the h (rightmost) file 
+        if(kingBitboard & NOT_H_FILE){
+
+            //Shift the bit on the  king bitboard up one row and one square to the right, then add that bit to the attacks bitboard
+            attacksBitboard |= kingBitboard >> 7;
+
+            //Shift the bit on the  king bitboard down one row and one square to the right, then add that bit to the attacks bitboard
+            attacksBitboard |= kingBitboard << 9;
+
+            //Shift the bit on the  king bitboard one square to the right, then add that bit to the attacks bitboard
+            attacksBitboard |= kingBitboard << 1;
+        }
+
+        //If the knight is not located on the a (leftmost) file 
+        if(kingBitboard & NOT_A_FILE){
+
+            //Shift the bit on the  king bitboard up one row and one square to the left, then add that bit to the attacks bitboard
+            attacksBitboard |= kingBitboard >> 9;
+
+            //Shift the bit on the  king bitboard down one row and one square to the left, then add that bit to the attacks bitboard
+            attacksBitboard |= kingBitboard << 7;
+
+            //Shift the bit on the  king bitboard one square to the left, then add that bit to the attacks bitboard
+            attacksBitboard |= kingBitboard >> 1;
+        }
+
+        return attacksBitboard;
+
+    }
+
+    //Generate a custom file-rank mask
+    U64 generateMask(int fileNumber, int rankNumber){
+
+        //Initialise an emty mask
+        U64 mask = 0ULL;
+
+        //Loop over ranks
+        for(int rank = 0; rank < 8; rank++){
+
+            //Loop over files
+            for(int file = 0; file < 8; file++){
+
+                //Calculate a square index
+                int squareIndex = rank * 8 + file;
+
+                //If the given file has been reached
+                if(fileNumber != -1 && file == fileNumber){
+                    //Set a bit on the current square index
+                    setBit(mask, squareIndex);
+                }
+
+                //If the given rank has been reached
+                if(rankNumber != -1 && rank == rankNumber){
+                    //Set a bit on the current square index
+                    setBit(mask, squareIndex);
+                }
+
+            }
+
+        }
+
+        return mask;
+
+    }
+    
 }
