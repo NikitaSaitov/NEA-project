e620bc0fc4be34bfa510870f44bd6a6a4b1404ce
NikitaSaitov <126459734+NikitaSaitov@users.noreply.github.com>
2023-05-04 09:21:41 +0100

    Ensure WASM support

--- AttackTable.cpp
+++ AttackTable.cpp
@@ -1,95 +1,134 @@
 #include "AttackTable.h"
 #include "bitboard_operations.h"
-#include "magics.h"
 #include "engine_exceptions.h"
+#include "magic_numbers.h"
+#include "masks.h"
 #include "const.h"
 
-enum {white, black, both};
+extern "C" {
 
-void AttackTable::initializeMagicNumbers(){
+    enum {white, black, both};
 
-    for(int squareIndex = 0; squareIndex < 64; squareIndex++){
-        bishopMagics[squareIndex] = findMagicNumber(squareIndex, BISHOP_RELEVANT_BITS[squareIndex], true);
-        rookMagics[squareIndex] = findMagicNumber(squareIndex, ROOK_RELEVANT_BITS[squareIndex], false);
-    }     
-}
+    void AttackTable::initialiseMagicNumbers(){
 
-void AttackTable::initializeLeapingPieceTables(){
+        //Loop over the squares 
+        for(int squareIndex = 0; squareIndex < 64; squareIndex++){
 
-    if(bishopMagics[0] == 0 || rookMagics[0] == 0){
-        throw MagicNumberNotInitializedException();
-    }
+            //Initialise magic numbers for bishops
+            bishopMagics[squareIndex] = findMagicNumber(squareIndex, BISHOP_RELEVANT_BITS[squareIndex], true);
 
-    for(int squareIndex = 0; squareIndex < 64; squareIndex++){
+            //Initialise magic numbers for rooks
+            rookMagics[squareIndex] = findMagicNumber(squareIndex, ROOK_RELEVANT_BITS[squareIndex], false);
 
-        pawnAttacks[white][squareIndex] = maskPawnAttacks(squareIndex, white);
-        pawnAttacks[black][squareIndex] = maskPawnAttacks(squareIndex, black);
-        knightAttacks[squareIndex] = maskKnightAttacks(squareIndex);
-        kingAttacks[squareIndex] = maskKingAttacks(squareIndex);
+        }    
 
     }
-}
 
-void AttackTable::initializeSlidingPieceTables(bool fBishop){
+    void AttackTable::initialiseLeapingPieceTables(){
 
-    for(int squareIndex = 0; squareIndex < 64; squareIndex++){
+        //Loop over the squares 
+        for(int squareIndex = 0; squareIndex < 64; squareIndex++){
 
-        bishopMasks[squareIndex] = maskBishopAttacks(squareIndex);
-        rookMasks[squareIndex] = maskRookAttacks(squareIndex);
+            //Initialise leaping piece atttacks//
+            pawnAttacks[white][squareIndex] = maskPawnAttacks(squareIndex, white);
+            pawnAttacks[black][squareIndex] = maskPawnAttacks(squareIndex, black);
+            knightAttacks[squareIndex] = maskKnightAttacks(squareIndex);
+            kingAttacks[squareIndex] = maskKingAttacks(squareIndex);
 
-        U64 attackMask = fBishop ? bishopMasks[squareIndex] : rookMasks[squareIndex];
-
-        int relevantBits = getPopulationCount(attackMask);
-        int maxOccupancyIndex = 1 << relevantBits;
-
-        for(int occupancyIndex = 0; occupancyIndex < maxOccupancyIndex; occupancyIndex++){
-
-            U64 occupancy = generateOccupancy(occupancyIndex, relevantBits, attackMask);
-                
-            if(fBishop){
-
-                int magicIndex = (occupancy * bishopMagics[squareIndex]) >> (64 - BISHOP_RELEVANT_BITS[squareIndex]);
-                bishopAttacks[squareIndex][magicIndex] = generateBishopAttacks(squareIndex, occupancy);
-
-            }
-            else{
-
-                int magicIndex = (occupancy * rookMagics[squareIndex]) >> (64 - ROOK_RELEVANT_BITS[squareIndex]);
-                rookAttacks[squareIndex][magicIndex] = generateRookAttacks(squareIndex, occupancy);
-
-            }
         }
+
     }
+
+    void AttackTable::initialiseSlidingPieceTables(bool fBishop){
+
+        //Check if the magic numbers are initialised
+        if(!bishopMagics[0] || !rookMagics[0]){
+            throw MagicNumberNotInitialisedException();
+        }
+
+        //Loop over the squares
+        for(int squareIndex = 0; squareIndex < 64; squareIndex++){
+
+            //Fill the arrays of masks to fetch attacks later
+            bishopMasks[squareIndex] = maskBishopAttacks(squareIndex);
+            rookMasks[squareIndex] = maskRookAttacks(squareIndex);
+
+            //Assign the attack mask
+            U64 attackMask = fBishop ? bishopMasks[squareIndex] : rookMasks[squareIndex];
+
+            int relevantBits = getPopulationCount(attackMask);
+            int maxOccupancyIndex = 1 << relevantBits;
+
+            //Loop over the occupancy indicies
+            for(int occupancyIndex = 0; occupancyIndex < maxOccupancyIndex; occupancyIndex++){
+
+                //Get an occupancy bitboard from the current occupancy index
+                U64 occupancy = getOccupancyFromIndex(occupancyIndex, relevantBits, attackMask);
+                    
+                if(fBishop){
+
+                    //Generate a magicIndex
+                    int magicIndex = (occupancy * bishopMagics[squareIndex]) >> (64 - BISHOP_RELEVANT_BITS[squareIndex]);
+
+                    //Fill the bishop attacks array
+                    bishopAttacks[squareIndex][magicIndex] = generateBishopAttacks(squareIndex, occupancy);
+
+                }else{
+
+                    //Generate a magicIndex
+                    int magicIndex = (occupancy * rookMagics[squareIndex]) >> (64 - ROOK_RELEVANT_BITS[squareIndex]);
+
+                    //Fill the rook attacks array
+                    rookAttacks[squareIndex][magicIndex] = generateRookAttacks(squareIndex, occupancy);
+
+                }
+
+            }
+
+        }
+
+    }
+
+    const U64 AttackTable::getPawnAttacks(int sideToMove, int squareIndex){
+        //Fetch the attacks
+        return pawnAttacks[sideToMove][squareIndex];
+    }
+
+    const U64 AttackTable::getKnightAttacks(int squareIndex){
+        //Fetch the attacks
+        return knightAttacks[squareIndex];
+    }
+
+    const U64 AttackTable::getKingAttacks(int squareIndex){
+        //Fetch the attacks
+        return kingAttacks[squareIndex];
+    }
+
+    const U64 AttackTable::getBishopAttacks(int squareIndex, U64 occupancy){
+
+        //Convert the occupancy into the index of the attack table 
+        occupancy &= bishopMasks[squareIndex]; 
+        occupancy *= bishopMagics[squareIndex];
+        occupancy >>= 64 - BISHOP_RELEVANT_BITS[squareIndex];
+
+        //Fetch the attacks
+        return bishopAttacks[squareIndex][occupancy];
+    }
+
+    const U64 AttackTable::getRookAttacks(int squareIndex, U64 occupancy){
+        
+        //Convert the occupancy into the index of the attack table 
+        occupancy &= rookMasks[squareIndex]; 
+        occupancy *= rookMagics[squareIndex];
+        occupancy >>= 64 - ROOK_RELEVANT_BITS[squareIndex];
+
+        //Fetch the attacks
+        return rookAttacks[squareIndex][occupancy];
+    }
+
+    const U64 AttackTable::getQueenAttacks(int squareIndex, U64 occupancy){
+        //Logical AND on the bishop and rook attacks
+        return getBishopAttacks(squareIndex, occupancy) | getRookAttacks(squareIndex, occupancy);
+    }  
+
 }
-
-const U64 AttackTable::getPawnAttacks(int sideToMove, int squareIndex){
-    return pawnAttacks[sideToMove][squareIndex];
-}
-
-const U64 AttackTable::getKnightAttacks(int squareIndex){
-    return knightAttacks[squareIndex];
-}
-
-const U64 AttackTable::getKingAttacks(int squareIndex){
-    return kingAttacks[squareIndex];
-}
-
-const U64 AttackTable::getBishopAttacks(int squareIndex, U64 occupancy){
-
-    occupancy &= bishopMasks[squareIndex]; 
-    occupancy *= bishopMagics[squareIndex];
-    occupancy >>= 64 - BISHOP_RELEVANT_BITS[squareIndex];
-    return bishopAttacks[squareIndex][occupancy];
-}
-
-const U64 AttackTable::getRookAttacks(int squareIndex, U64 occupancy){
-    
-    occupancy &= rookMasks[squareIndex]; 
-    occupancy *= rookMagics[squareIndex];
-    occupancy >>= 64 - ROOK_RELEVANT_BITS[squareIndex];
-    return rookAttacks[squareIndex][occupancy];
-}
-
-const U64 AttackTable::getQueenAttacks(int squareIndex, U64 occupancy){
-    return getBishopAttacks(squareIndex, occupancy) | getRookAttacks(squareIndex, occupancy);
-}  
